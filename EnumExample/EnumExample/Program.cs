f3c708961e5d04dbc33d8fcbb1e240136ca7fe92
phyuthwekhaing <phyuthwekhaing@users.noreply.github.com>
2023-08-29 23:27:27 +0100

    Enum example

---
+++ EnumExample/EnumExample/Program.cs
@@ -0,0 +1,18 @@
+﻿public enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
+
+public class EnumExample
+{
+    public static void Main()
+    {
+        Season currentSeason = Season.Summer;
+        Console.WriteLine($"Current season is {currentSeason}.");
+        Season nextSeason = currentSeason + 1;
+        Console.WriteLine($"Next Season is {nextSeason}.");
+    }
+}
